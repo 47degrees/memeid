d2d1de71463f9883dee0cef288d5eae40f665c9a
Alejandro Hernández <info@alejandrohdezma.com>
2019-12-13 12:36:00 +0100

    Add laws test for UUID

--- src/test/scala/memeid/package.scala
+++ src/test/scala/memeid/package.scala
@@ -3,7 +3,11 @@ import org.scalacheck.Arbitrary.arbitrary
 
 package object memeid {
 
-  implicit val UUIDGenInstance: Arbitrary[UUID] = Arbitrary {
+  implicit val UUID2UUIDArbitraryInstance: Arbitrary[UUID => UUID] = Arbitrary(
+    arbitrary[UUID].map(uuid => { _: UUID => uuid })
+  )
+
+  implicit val UUIDArbitraryInstance: Arbitrary[UUID] = Arbitrary {
     for {
       msb <- arbitrary[Long]
       lsb <- arbitrary[Long]
@@ -11,27 +15,27 @@ package object memeid {
   }
 
   @SuppressWarnings(Array("scalafix:DisableSyntax.isInstanceOf"))
-  implicit val UUIDV1GenInstance: Arbitrary[UUID.V1] = Arbitrary {
+  implicit val UUIDV1ArbitraryInstance: Arbitrary[UUID.V1] = Arbitrary {
     arbitrary[UUID].retryUntil(_.isInstanceOf[UUID.V1]).map(uuid => new UUID.V1(uuid.juuid))
   }
 
   @SuppressWarnings(Array("scalafix:DisableSyntax.isInstanceOf"))
-  implicit val UUIDV2GenInstance: Arbitrary[UUID.V2] = Arbitrary {
+  implicit val UUIDV2ArbitraryInstance: Arbitrary[UUID.V2] = Arbitrary {
     arbitrary[UUID].retryUntil(_.isInstanceOf[UUID.V2]).map(uuid => new UUID.V2(uuid.juuid))
   }
 
   @SuppressWarnings(Array("scalafix:DisableSyntax.isInstanceOf"))
-  implicit val UUIDV3GenInstance: Arbitrary[UUID.V3] = Arbitrary {
+  implicit val UUIDV3ArbitraryInstance: Arbitrary[UUID.V3] = Arbitrary {
     arbitrary[UUID].retryUntil(_.isInstanceOf[UUID.V3]).map(uuid => new UUID.V3(uuid.juuid))
   }
 
   @SuppressWarnings(Array("scalafix:DisableSyntax.isInstanceOf"))
-  implicit val UUIDV4GenInstance: Arbitrary[UUID.V4] = Arbitrary {
+  implicit val UUIDV4ArbitraryInstance: Arbitrary[UUID.V4] = Arbitrary {
     arbitrary[UUID].retryUntil(_.isInstanceOf[UUID.V4]).map(uuid => new UUID.V4(uuid.juuid))
   }
 
   @SuppressWarnings(Array("scalafix:DisableSyntax.isInstanceOf"))
-  implicit val UUIDV5GenInstance: Arbitrary[UUID.V5] = Arbitrary {
+  implicit val UUIDV5ArbitraryInstance: Arbitrary[UUID.V5] = Arbitrary {
     arbitrary[UUID].retryUntil(_.isInstanceOf[UUID.V5]).map(uuid => new UUID.V5(uuid.juuid))
   }
 
