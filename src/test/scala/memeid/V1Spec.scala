44592e0d9e366b0bc39a7c2fe2c733eb2a93de42
Alejandro Gomez <alejandro.gomez@47deg.com>
2019-12-27 14:35:50 +0100

    Recover commented tests

--- src/test/scala/memeid/V1Spec.scala
+++ src/test/scala/memeid/V1Spec.scala
@@ -1,10 +1,18 @@
 package memeid
 
+import java.util.{UUID => JUUID}
+
+import scala.concurrent.duration._
+
 import cats.data._
 import cats.effect._
+import cats.syntax.apply._
+import cats.syntax.flatMap._
+import cats.syntax.functor._
 import cats.syntax.order._
 import cats.syntax.parallel._
 
+import memeid.JavaConverters._
 import org.specs2.ScalaCheck
 import org.specs2.matcher.IOMatchers
 import org.specs2.mutable.Specification
@@ -27,26 +35,25 @@ class V1Spec extends Specification with ScalaCheck with IOMatchers {
       io must returnValue(ids.size)
     }
 
-    //"not generate the same UUID twice with high concurrency" in {
-    //  @SuppressWarnings(Array("scalafix:Disable.get"))
-    //  def ids = NonEmptyList.fromList(List.range(1, 999)).get
-    //  val io  = ids.parTraverse(_ => UUID.v1[IO]).unsafeRunSync.toList
-    //  io.toSet.size must be equalTo ids.size
-    //}
+    "not generate the same UUID twice with high concurrency" in {
+      @SuppressWarnings(Array("scalafix:Disable.get"))
+      def ids = NonEmptyList.fromList(List.range(1, 999)).get
+      val io  = ids.parTraverse(_ => UUID.v1[IO]).map(_.toList.size)
+      io must returnValue(ids.size)
+    }
 
-    //// todo: more thorough testing of this
-    //def bench[F[_]: Sync: Clock, A](f: F[A]): F[(Long, A)] =
-    //  for {
-    //    start <- Clock[F].monotonic(NANOSECONDS)
-    //    a     <- f
-    //    end   <- Clock[F].monotonic(NANOSECONDS)
-    //  } yield (end - start, a)
+    def bench[F[_]: Sync: Clock, A](f: F[A]): F[(Long, A)] =
+      for {
+        start <- Clock[F].monotonic(NANOSECONDS)
+        a     <- f
+        end   <- Clock[F].monotonic(NANOSECONDS)
+      } yield (end - start, a)
 
-    //"be faster than random generation" in {
-    //  val random: IO[UUID] = IO.delay(JUUID.randomUUID.asScala)
-    //  val v1: IO[UUID]     = UUID.v1[IO]
-    //  val ((r, _), (v, _)) = (bench(random), bench(v1)).tupled.unsafeRunSync
-    //  r must be greaterThan v
-    //}
+    "be faster than random generation" in {
+      val random: IO[UUID] = IO.delay(JUUID.randomUUID.asScala)
+      val v1: IO[UUID]     = UUID.v1[IO]
+      val ((r, _), (v, _)) = (bench(random), bench(v1)).tupled.unsafeRunSync
+      r must be greaterThan v
+    }
   }
 }
