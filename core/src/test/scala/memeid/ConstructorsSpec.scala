39a67564f21e95bbb3ec41321fd4784cd135f1e7
Alejandro Hernández <info@alejandrohdezma.com>
2020-01-02 17:14:27 +0100

    Remove uneeded cats import

--- core/src/test/scala/memeid/ConstructorsSpec.scala
+++ core/src/test/scala/memeid/ConstructorsSpec.scala
@@ -1,17 +1,16 @@
 package memeid
 
-import cats.implicits._
-
 import org.scalacheck.Gen
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 
+@SuppressWarnings(Array("scalafix:Disable.toString"))
 class ConstructorsSpec extends Specification with ScalaCheck {
 
   "from" should {
 
     "return Some on valid UUID" in prop { uuid: UUID =>
-      UUID.from(uuid.show) must beRight(uuid)
+      UUID.from(uuid.toString) should beRight(uuid)
     }
 
     "return None on invalid string" in prop { s: String =>
