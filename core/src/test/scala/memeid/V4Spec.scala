6e35f931576a8019134d500f54e98cd225d1c2d0
Alejandro Hernández <info@alejandrohdezma.com>
2020-01-07 17:38:25 +0100

    Use `Disable.scala.parallel`

--- core/src/test/scala/memeid/V4Spec.scala
+++ core/src/test/scala/memeid/V4Spec.scala
@@ -19,7 +19,7 @@ package memeid
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 
-@SuppressWarnings(Array("scalafix:Disable.map", "scalafix:Disable.to"))
+@SuppressWarnings(Array("scalafix:Disable.scala.parallel"))
 class V4Spec extends Specification with ScalaCheck {
 
   "V4 constructor" should {
