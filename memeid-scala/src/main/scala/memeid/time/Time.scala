8ebd42ffb4b0e0a679bcb6a35cbddc544fc89f5f
Alejandro Hernández <info@alejandrohdezma.com>
2020-01-31 17:16:17 +0100

    Move `Time` to Java

--- memeid-scala/src/main/scala/memeid/time/Time.scala
+++ memeid-scala/src/main/scala/memeid/time/Time.scala
@@ -16,9 +16,7 @@
 
 package memeid.time
 
-import java.util.concurrent.atomic.AtomicReference
-
-import scala.annotation.tailrec
+import memeid.Timestamp
 
 trait Time {
 
@@ -27,51 +25,11 @@ trait Time {
 
 }
 
-@SuppressWarnings(Array("scalafix:DisableSyntax.!="))
 object Time {
-
-  def current: Long = System.currentTimeMillis
-
-  /* Class to ensure unique gregorian timestamps for a given `resolution`. It works by keeping a sequence of
-   * generated IDs and shifting the `ts` value adding the `seq`. Users of this class must ensure to
-   * stall timestamp generation if the seqids are all used for the given `resolution`, since `State#update`
-   * will block until it finds a different timestamp to generate new monotonic values. */
-  final case class State(seq: Short, ts: Long, resolution: Short) {
-    /* Advance to next state, upping the sequence number. */
-    private def next: State = State((seq + 1).toShort, ts, resolution)
-
-    /* Reset the state with a new tick. */
-    private def reset(newTs: Long): State =
-      copy(seq = 0, ts = newTs)
-
-    /* Update the state with a new tick. */
-    @tailrec
-    def update(newTs: Long): State =
-      if (ts != newTs) {
-        reset(newTs)
-      } else if (seq < resolution) {
-        next
-      } else {
-        update(current)
-      }
-
-    /* Compute the gregorian time monotonic timestamp for this state. */
-    def timestamp: Long =
-      seq + 100103040000000000L + (1000 * (ts + 2208988800000L))
-  }
-
-  object State {
-    val empty: State = State(0, 0, 999)
-  }
-
-  /* We ensure atomic updates to the state with no locks using an atomic reference, which allows
-   * us to stall timestamp generation if many concurrent timestamps are generated. */
-  val state: AtomicReference[State] = new AtomicReference[State](State.empty)
 
   implicit def apply: Time = new Time {
 
-    override def monotonic: Long =
-      state.updateAndGet(_.update(current)).timestamp
+    override def monotonic: Long = Timestamp.monotonic()
 
   }
 }
---
+++ memeid/src/main/java/memeid/Timestamp.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2019-2020 47 Degrees, LLC. <http://www.47deg.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package memeid;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * Class to ensure unique gregorian timestamps. It works by keeping a sequence of
+ * generated IDs and shifting the `millis` value adding the `stamp`.
+ */
+public final class Timestamp {
+
+    /**
+     * Returns a gregorian time monotonic timestamp.
+     *
+     * @return a gregorian time monotonic timestamp
+     */
+    public static long monotonic() {
+        final State s = state.updateAndGet(state -> {
+            long newMillis;
+            do {
+                newMillis = System.currentTimeMillis();
+                if (state.millis != newMillis) {
+                    return new State(0, newMillis);
+                } else if (state.stamp < 999) {
+                    return new State(state.stamp + 1, state.millis);
+                }
+            } while (true);
+        });
+
+        return s.stamp + 100103040000000000L + (1000 * (s.millis + 2208988800000L));
+    }
+
+    private static final class State {
+        private final int stamp;
+        private final long millis;
+
+        private State(int stamp, long millis) {
+            this.stamp = stamp;
+            this.millis = millis;
+        }
+    }
+
+    private final static AtomicReference<State> state = new AtomicReference<>(new State(0, 0));
+
+}
