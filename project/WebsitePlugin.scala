e7691a656fa6836dfa692d2c50b06d2f58398b8e
Alejandro Hernández <info@alejandrohdezma.com>
2020-06-02 07:57:28 +0200

    Remove unused imports

--- project/WebsitePlugin.scala
+++ project/WebsitePlugin.scala
@@ -1,22 +1,22 @@
 import mdoc.MdocPlugin
 import microsites.MicrositesPlugin
 import microsites.MicrositesPlugin.autoImport._
-import com.typesafe.sbt.site.SitePlugin.autoImport._
-import sbt.Keys._
 import sbt._
 
 object WebsitePlugin extends AutoPlugin {
+
   override def requires: Plugins = MdocPlugin && MicrositesPlugin
 
   override def trigger: PluginTrigger = allRequirements
 
   override def projectSettings: Seq[Def.Setting[_]] =
     Seq(
-      micrositeName                := "memeid",
-      micrositeBaseUrl             := "memeid",
-      micrositeDocumentationUrl    := "docs",
-      micrositeGithubToken         := Option(System.getenv().get("GITHUB_TOKEN")),
-      micrositePushSiteWith        := GitHub4s,
-      micrositeHighlightLanguages  := Seq("java", "scala")
+      micrositeName               := "memeid",
+      micrositeBaseUrl            := "memeid",
+      micrositeDocumentationUrl   := "docs",
+      micrositeGithubToken        := Option(System.getenv().get("GITHUB_TOKEN")),
+      micrositePushSiteWith       := GitHub4s,
+      micrositeHighlightLanguages := Seq("java", "scala")
     )
+
 }
