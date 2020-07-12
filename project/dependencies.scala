ba0fada0a27fa612a1068b38d4871aeb8589b7dc
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-07-12 22:10:00 +0200

    Update cats-effect to 2.1.4 (#206)

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -83,7 +83,7 @@ object dependencies extends AutoPlugin {
   )
 
   private val documentation = Seq(
-    "org.typelevel"  %% "cats-effect" % "2.1.3",
+    "org.typelevel"  %% "cats-effect" % "2.1.4",
     "io.circe"       %% "circe-core"  % "0.13.0",
     "org.tpolecat"   %% "doobie-h2"   % "0.9.0",
     "org.http4s"     %% "http4s-dsl"  % "0.21.6",
