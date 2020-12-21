bd8fa47f81ebcad2d4715ab6a155b9d6dfba27c6
47erbot <developer@47deg.com>
2020-12-21 05:09:16 +0000

    Update cats-effect, cats-laws to 2.3.1

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -48,7 +48,7 @@ object dependencies extends AutoPlugin {
   private val cats = Def.setting {
     Seq(
       "org.typelevel" %% "cats-effect"       % V.`cats-effect`.value % Provided,
-      "org.typelevel" %% "cats-laws"         % "2.3.0"               % Test,
+      "org.typelevel" %% "cats-laws"         % "2.3.1"               % Test,
       "org.typelevel" %% "discipline-specs2" % "1.1.2"               % Test,
       "org.specs2"    %% "specs2-cats"       % "4.10.5"              % Test
     )
@@ -104,7 +104,7 @@ object dependencies extends AutoPlugin {
   )
 
   private val documentation = Seq(
-    "org.typelevel"               %% "cats-effect" % "2.3.0",
+    "org.typelevel"               %% "cats-effect" % "2.3.1",
     "io.circe"                    %% "circe-core"  % "0.13.0",
     "org.tpolecat"                %% "doobie-h2"   % "0.9.4",
     "org.http4s"                  %% "http4s-dsl"  % "0.21.13",
