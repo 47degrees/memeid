dfbd8a902888707399259d691ad953248b6edb05
Alejandro Hernández <info@alejandrohdezma.com>
2021-02-13 13:25:48 +0100

    Update fuuid to 0.5.0

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -110,7 +110,7 @@ object dependencies extends AutoPlugin {
     "org.http4s"                  %% "http4s-dsl"  % "0.21.18",
     "org.scalacheck"              %% "scalacheck"  % "1.15.2",
     "com.softwaremill.sttp.tapir" %% "tapir-core"  % "0.17.10",
-    "io.chrisdavenport"           %% "fuuid"       % "0.4.0"
+    "io.chrisdavenport"           %% "fuuid"       % "0.5.0"
   )
 
   override def trigger: PluginTrigger = allRequirements
