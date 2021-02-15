6a51e54ab9e924fbce934dbf3ca29355a52c79b1
47erbot <developer@47deg.com>
2021-02-15 06:13:28 +0100

    Update cats-laws to 2.4.1

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -48,7 +48,7 @@ object dependencies extends AutoPlugin {
   private val cats = Def.setting {
     Seq(
       "org.typelevel" %% "cats-effect"       % V.`cats-effect`.value % Provided,
-      "org.typelevel" %% "cats-laws"         % "2.3.1"               % Test,
+      "org.typelevel" %% "cats-laws"         % "2.4.1"               % Test,
       "org.typelevel" %% "discipline-specs2" % "1.1.3"               % Test,
       "org.specs2"    %% "specs2-cats"       % "4.10.6"              % Test
     )
