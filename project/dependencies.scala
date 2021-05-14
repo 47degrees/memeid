4425d8d52e22a0ca30286192df084110aba82674
47erbot <developer@47deg.com>
2021-05-14 05:18:25 +0000

    Update scala-parallel-collections to 1.0.3

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -40,7 +40,7 @@ object dependencies extends AutoPlugin {
   private val common = Seq("org.specs2" %% "specs2-scalacheck" % "4.11.0" % Test)
 
   private val parallel = on(2, 13) {
-    "org.scala-lang.modules" %% "scala-parallel-collections" % "1.0.2" % Test
+    "org.scala-lang.modules" %% "scala-parallel-collections" % "1.0.3" % Test
   }
 
   private val cats = Def.setting {
