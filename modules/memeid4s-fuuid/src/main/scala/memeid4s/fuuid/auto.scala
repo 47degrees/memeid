4e30c3cecf4c23a5ebf57ab94f61ac9581cab317
47erbot <developer@47deg.com>
2021-07-05 07:20:45 +0200

    Update sbt-scalafmt-defaults to 0.4.3

--- modules/memeid4s-fuuid/src/main/scala/memeid4s/fuuid/auto.scala
+++ modules/memeid4s-fuuid/src/main/scala/memeid4s/fuuid/auto.scala
@@ -22,14 +22,10 @@ import memeid4s.UUID
 @SuppressWarnings(Array("scalafix:DisableSyntax.implicitConversion"))
 object auto {
 
-  /**
-   * Provides an automatic conversion between a `FUUID` and a memeid's `UUID`.
-   */
+  /** Provides an automatic conversion between a `FUUID` and a memeid's `UUID`. */
   implicit def FUUID2UUIDOps(fuuid: FUUID): UUID = UUID.fromUUID(FUUID.Unsafe.toUUID(fuuid))
 
-  /**
-   * Provides an automatic conversion between a memeid's `UUID` and a `FUUID`.
-   */
+  /** Provides an automatic conversion between a memeid's `UUID` and a `FUUID`. */
   implicit def UUID2FUUIDOps(uuid: UUID): FUUID = FUUID.fromUUID(uuid.asJava())
 
 }
--- modules/memeid4s-tapir/src/main/scala/memeid4s/tapir/instances.scala
+++ modules/memeid4s-tapir/src/main/scala/memeid4s/tapir/instances.scala
@@ -26,17 +26,13 @@ import sttp.tapir.SchemaType.SString
 @SuppressWarnings(Array("scalafix:DisableSyntax.valInAbstract"))
 trait instances {
 
-  /**
-   * Allows reading `UUID` values from path, query params, or headers
-   */
+  /** Allows reading `UUID` values from path, query params, or headers */
   implicit val UUIDCodec: Codec[String, UUID, CodecFormat.TextPlain] =
     Codec.string
       .mapDecode(s => UUID.from(s).fold(DecodeResult.Error(s, _), DecodeResult.Value(_)))(_.toString) // scalafix:ok
       .modifySchema(_.format("uuid"))
 
-  /**
-   * Provides a valid `Schema` for `UUID` type, to be used when generating documentation.
-   */
+  /** Provides a valid `Schema` for `UUID` type, to be used when generating documentation. */
   implicit val UUIDSchema: Schema[UUID] = Schema(SString).format("uuid")
 
 }
