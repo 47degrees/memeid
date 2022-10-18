164452d7372e8aa4345dde07a22e430808be2837
Jose Gutierrez <gutiory@gmail.com>
2022-10-18 12:17:52 +0200

    Run scalafmt

--- modules/memeid/src/main/java/memeid/Offset.java
+++ modules/memeid/src/main/java/memeid/Offset.java
@@ -18,20 +18,20 @@ package memeid;
 
 public final class Offset {
 
-    public final static long OFFSET_56 = 56L;
-    public final static long OFFSET_48 = 48L;
+	public final static long OFFSET_56 = 56L;
+	public final static long OFFSET_48 = 48L;
 
-    public final static long TIME_LOW = 0L;
-    public final static long TIME_MID = 32L;
-    public final static long TIME_HIGH = 48L;
+	public final static long TIME_LOW = 0L;
+	public final static long TIME_MID = 32L;
+	public final static long TIME_HIGH = 48L;
 
-    public final static long CLOCK_SEQ_LOW = 0L;
-    public final static long CLOCK_SEQ_HIGH = 8L;
+	public final static long CLOCK_SEQ_LOW = 0L;
+	public final static long CLOCK_SEQ_HIGH = 8L;
 
 	public final static long HASHED = 62L;
 
-    public final static long V4_LSB = 62L;
+	public final static long V4_LSB = 62L;
 
-    public final static long VERSION = 12L;
-    
+	public final static long VERSION = 12L;
+
 }
