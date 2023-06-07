04653d12e2aba3b13d13949f92b41940489a1b71
47erbot <developer@47deg.com>
2023-06-07 07:18:33 +0200

    Update tapir-core, tapir-openapi-docs to 1.5.1

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -62,8 +62,8 @@ object dependencies extends AutoPlugin {
   )
 
   val tapir: Seq[ModuleID] = Seq(
-    "com.softwaremill.sttp.tapir"   %% "tapir-core"         % "1.5.0",
-    "com.softwaremill.sttp.tapir"   %% "tapir-openapi-docs" % "1.5.0" % Test,
+    "com.softwaremill.sttp.tapir"   %% "tapir-core"         % "1.5.1",
+    "com.softwaremill.sttp.tapir"   %% "tapir-openapi-docs" % "1.5.1" % Test,
     "com.softwaremill.sttp.apispec" %% "openapi-circe-yaml" % "0.4.0" % Test
   )
 
@@ -81,7 +81,7 @@ object dependencies extends AutoPlugin {
     "org.tpolecat"                %% "doobie-h2"   % "1.0.0-RC2",
     "org.http4s"                  %% "http4s-dsl"  % "0.23.19",
     "org.scalacheck"              %% "scalacheck"  % "1.17.0",
-    "com.softwaremill.sttp.tapir" %% "tapir-core"  % "1.5.0",
+    "com.softwaremill.sttp.tapir" %% "tapir-core"  % "1.5.1",
     "io.chrisdavenport"           %% "fuuid"       % "0.8.0-M2"
   )
 
