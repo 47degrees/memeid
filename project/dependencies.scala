a7b8a51f48178b93d4e16ef16cc7e6178023fdda
fortyseven_degrees <developer@47deg.com>
2023-11-22 05:10:00 +0000

    Update openapi-circe-yaml to 0.7.2

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -64,7 +64,7 @@ object dependencies extends AutoPlugin {
   val tapir: Seq[ModuleID] = Seq(
     "com.softwaremill.sttp.tapir"   %% "tapir-core"         % "1.9.0",
     "com.softwaremill.sttp.tapir"   %% "tapir-openapi-docs" % "1.9.0" % Test,
-    "com.softwaremill.sttp.apispec" %% "openapi-circe-yaml" % "0.7.1" % Test
+    "com.softwaremill.sttp.apispec" %% "openapi-circe-yaml" % "0.7.2" % Test
   )
 
   val fuuid: Seq[ModuleID] = Seq(
